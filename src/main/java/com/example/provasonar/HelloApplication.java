934f1493787199b395588d98d42d1079418cf24e
Agnese021 <agneseagueli@gmail.com>
2023-02-13 16:27:39 +0100

    prova1

--- src/main/java/com/example/provasonar/HelloApplication.java
+++ src/main/java/com/example/provasonar/HelloApplication.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    public static final int a = 3;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -19,7 +20,7 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello" + a);
         launch();
     }
 }
